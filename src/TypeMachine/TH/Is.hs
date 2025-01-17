bded9c1cb978ba59aca163dbab9b47734bdcd9c6
Arthur Jamet <arthur.jamet@gmail.com>
2025-01-17 15:55:52 +0000

    deriveIs produces the 'to' function

--- src/TypeMachine/TH/Is.hs
+++ src/TypeMachine/TH/Is.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 
-module TypeMachine.TH.Is (isClassName, deriveIs, defineIs) where
+module TypeMachine.TH.Is (isClassName, toFuncName, deriveIs, defineIs) where
 
 import Control.Monad (MonadPlus (mzero), forM)
 import qualified Data.Map.Strict as Map
@@ -19,7 +20,16 @@ import TypeMachine.Type (fields, reifyType)
 --   IsUser
 -- @
 isClassName :: Name -> Name
-isClassName tyName = mkName $ "Is" ++ capitalize (nameBase tyName)
+isClassName = mkName . ("Is" ++) . capitalize . nameBase
+
+-- | Get the name of the 'To' function generated for the given type
+--
+-- @
+-- > toFuncName ''User
+--   ToUser
+-- @
+toFuncName :: Name -> Name
+toFuncName = mkName . ("to" ++) . capitalize . nameBase
 
 -- | Returns the declaration of the instance of 'Is' for a given type
 --
@@ -34,14 +44,14 @@ deriveIs sourceTypeName destTypeName = do
     destFields <- fields <$> reifyType destTypeName
     sourceFields <- fields <$> reifyType sourceTypeName
     let className = mkName ("Is" ++ nameBase sourceTypeName)
-    classFuncs <- forM (Map.toList sourceFields) $ \(n, (_, t)) -> case Map.lookup n destFields of
-        Just _ -> fieldNameToFunDec n
-        Nothing -> do
-            isOpt <- fieldIsOptional t
-            if isOpt
-                then fieldNameToMemptyFunDec n
-                else
-                    fail
+    classFuncs <- forM (Map.toList sourceFields) $ \(n, (_, t)) ->
+        case Map.lookup n destFields of
+            Just _ -> fieldNameToFunDec n
+            Nothing ->
+                ifM
+                    (fieldIsOptional t)
+                    (fieldNameToMemptyFunDec n)
+                    ( fail
                         ( printf
                             "Type-Machine Error: Cannot define instance of %s for %s. Field '%s' is missing in %s "
                             (nameBase className)
@@ -49,6 +59,7 @@ deriveIs sourceTypeName destTypeName = do
                             n
                             destTypeStr
                         )
+                    )
     return [InstanceD Nothing [] (AppT (ConT className) (ConT destTypeName)) classFuncs]
   where
     destTypeStr = nameBase destTypeName
@@ -65,8 +76,9 @@ deriveIs sourceTypeName destTypeName = do
     fieldIsOptional :: TH.Type -> Q Bool
     fieldIsOptional (AppT t _) = isInstance ''MonadPlus [t]
     fieldIsOptional _ = return False
+    ifM mbool t f = do bool <- mbool; if bool then t else f
 
--- | Define the 'Is' class for the given type
+-- | Define the 'Is' class for the given type and generate the 'To' function
 --
 -- @
 --  > data User = User { id :: Int, name :: String }
@@ -75,14 +87,19 @@ deriveIs sourceTypeName destTypeName = do
 --  class IsUser a where
 --       getId :: a -> Int
 --       getName :: a -> String
+--
+--  toUser :: (IsUser a) => a -> User
+--  toUser a = User (getId a) (getName a)
 -- @
 defineIs :: Name -> Q [Dec]
 defineIs tyName = do
     ty <- reifyType tyName
     classTypeVar <- newName "a"
-    let vbts = fields ty
-    let classFuncs = vbtToFunDec classTypeVar <$> Map.toList vbts
-    return [ClassD [] (isClassName tyName) [PlainTV classTypeVar BndrReq] [] classFuncs]
+    let classFuncs = vbtToFunDec classTypeVar <$> Map.toList (fields ty)
+    to <- defineTo tyName
+    return $
+        ClassD [] (isClassName tyName) [PlainTV classTypeVar BndrReq] [] classFuncs
+            : to
   where
     -- vbtToFunDec a id Int == getId :: a -> Int
     vbtToFunDec :: Name -> (String, BangType) -> Dec
@@ -91,6 +108,36 @@ defineIs tyName = do
             memberName = mkName $ fieldNameToIsFuncName n
          in
             SigD memberName (AppT (AppT ArrowT (VarT classtypeVar)) t)
+
+-- | Generate the 'To' function
+--
+-- @
+--  > data User = User { id :: Int, name :: String }
+--  > defineIs ''User
+--
+--  toUser :: (IsUser a) => a -> User
+--  toUser from = User (getId from) (getName from)
+-- @
+defineTo :: Name -> Q [Dec]
+defineTo tyName = do
+    ty <- reifyType tyName
+    toFuncType <-
+        sigD
+            toName
+            [t|forall a. ($(conT $ isClassName tyName) a) => a -> $(conT tyName)|]
+    toFuncBody <-
+        let
+            from = mkName "from"
+            app =
+                foldl'
+                    (\r n -> [|$r ($(varE $ mkName $ fieldNameToIsFuncName n) $(varE from))|])
+                    (conE $ mkName $ nameBase tyName)
+                    (Map.keys $ fields ty)
+         in
+            funD (toFuncName tyName) [clause [varP from] (normalB app) []]
+    return [toFuncType, toFuncBody]
+  where
+    toName = toFuncName tyName
 
 -- Internal
 fieldNameToIsFuncName :: String -> String
