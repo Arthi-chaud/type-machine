c32299c580eec49705314f9b46a961dbb9ac5e1f
Arthur Jamet <arthur.jamet@gmail.com>
2025-05-13 09:44:04 +0100

    Benchmarks: PSlitcases for build and traversals

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -1,52 +1,86 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-simplifiable-class-constraints #-}
 
-module Benchmark (benchmark) where
+module Benchmark (benchmarks) where
 
 import Control.Lens ((^.))
 import Criterion.Main
+import Data.Functor
 import Gen
+import SuperRecord (get)
 
-$(genTMRecord 20)
-$(genSuperrecord 20)
-$(genExtensible 20)
+$(genWithFieldCount 10)
+$(genWithFieldCount 20)
+$(genWithFieldCount 30)
 
-$(genTMRecord 30)
-$(genSuperrecord 30)
-$(genExtensible 30)
+benchmarks :: [Benchmark]
+benchmarks = [build, traversals]
 
-$(genTMRecord 40)
-$(genSuperrecord 40)
-$(genExtensible 40)
+data Case = TM | SuperRecord | Extensible
 
-benchmark :: Benchmark
-benchmark =
+buildRecord :: Case -> Int -> ()
+buildRecord c n = flip seq () $ case n of
+    10 -> case c of
+        TM -> field10_9 mkRecordTM10
+        SuperRecord -> get #fSR10_9 mkSuperRRecord10
+        Extensible -> mkExtensibleRecord10 ^. #fE10_9
+    20 -> case c of
+        TM -> field20_19 mkRecordTM20
+        SuperRecord -> get #fSR20_19 mkSuperRRecord20
+        Extensible -> mkExtensibleRecord20 ^. #fE20_19
+    30 -> case c of
+        TM -> field30_29 mkRecordTM30
+        SuperRecord -> get #fSR30_29 mkSuperRRecord30
+        Extensible -> mkExtensibleRecord30 ^. #fE30_29
+    _ -> error "unhandled n"
+
+build :: Benchmark
+build =
+    bgroup
+        "build"
+        $ depths <&> \n ->
+            bgroup
+                (show n)
+                ( cases <&> \(bname, case_) ->
+                    bench bname $
+                        nf
+                            (uncurry buildRecord)
+                            (case_, n)
+                )
+  where
+    depths = [10, 20, 30]
+    cases = [("tm", TM), ("superrecord", SuperRecord), ("extensible", Extensible)]
+
+traversals :: Benchmark
+traversals =
     bgroup
         "traverse"
         [ bgroup
+            "10"
+            [ bench "tm" $ nf sumRecordTM10 mkRecordTM10
+            , bench "superrecord" $ nf sumSuperRRecord10 mkSuperRRecord10
+            , bench "extensible" $ nf sumExtensibleRecord10 mkExtensibleRecord10
+            ]
+        , bgroup
             "20"
-            [ bench "tm" $ nf (\() -> sumRecordTM20 mkRecordTM20) ()
-            , bench "superrecord" $ nf (\() -> sumSuperRRecord20 mkSuperRRecord20) ()
-            , bench "extensible" $ nf (\() -> sumExtensibleRecord20 mkExtensibleRecord20) ()
+            [ bench "tm" $ nf sumRecordTM20 mkRecordTM20
+            , bench "superrecord" $ nf sumSuperRRecord20 mkSuperRRecord20
+            , bench "extensible" $ nf sumExtensibleRecord20 mkExtensibleRecord20
             ]
         , bgroup
             "30"
-            [ bench "tm" $ nf (\() -> sumRecordTM30 mkRecordTM30) ()
-            , bench "superrecord" $ nf (\() -> sumSuperRRecord30 mkSuperRRecord30) ()
-            , bench "extensible" $ nf (\() -> sumExtensibleRecord30 mkExtensibleRecord30) ()
-            ]
-        , bgroup
-            "40"
-            [ bench "tm" $ nf (\() -> sumRecordTM40 mkRecordTM40) ()
-            , bench "superrecord" $ nf (\() -> sumSuperRRecord40 mkSuperRRecord40) ()
-            , bench "extensible" $ nf (\() -> sumExtensibleRecord40 mkExtensibleRecord40) ()
+            [ bench "tm" $ nf sumRecordTM30 mkRecordTM30
+            , bench "superrecord" $ nf sumSuperRRecord30 mkSuperRRecord30
+            , bench "extensible" $ nf sumExtensibleRecord30 mkExtensibleRecord30
             ]
         ]
--- src/TypeMachine/TH.hs
+++ src/TypeMachine/TH.hs
@@ -5,7 +5,7 @@ import TypeMachine.TH.Is
 import TypeMachine.TM (TM, runTM)
 import TypeMachine.Type
 
--- | Entrypoint of TypeMachine. Create a new data type using a 'T' computation
+-- | Entrypoint of TypeMachine. Create a new data type using a 'TM' computation
 type_ :: String -> TM Type -> Q [Dec]
 type_ newTyName t = do
     fRes <- runTM t
